b8a40bbb778a8064b2494d4bfa89cff70b7e998c
Lang <transplus@foxmail.com>
2023-07-02 10:33:35 +0800

    example(echo server):A echo server and client using TcpClient and TcpServer to show the usage of This LanceNet library

--- apps/example/echo_client.cpp
+++ apps/example/echo_client.cpp
@@ -1,19 +1,104 @@
+// Author: Lance @ nwpu
+// echo_client that read user inputs and send to server
+
+#include "LanceNet/base/CountDownLatch.h"
+#include "LanceNet/base/Mutex.h"
+#include "LanceNet/base/Thread.h"
 #include "LanceNet/base/Time.h"
 #include "LanceNet/base/unix_wrappers.h"
 #include "LanceNet/net/Buffer.h"
 #include "LanceNet/net/EventLoop.h"
-#include "LanceNet/net/TcpConnection.h"
+#include <LanceNet/net/TcpConnection.h>
 #include <LanceNet/net/TcpServer.h>
+#include <LanceNet/net/TcpClient.h>
 #include <cstdio>
 #include <iostream>
 #include <string>
 #include <unistd.h>
 
+#include <iostream>
+using namespace std;
 using namespace LanceNet;
 using namespace net;
 
+using TcpConnectionPtr = TcpClient::TcpConnectionPtr;
+
+TcpConnectionPtr tcpconnection;
+MutexLock mutex;
+
+// wait for tcpconnectionPtr ready
+CountDownLatch latch(1);
+
+void OnMessage(const TcpConnectionPtr& connection, Buffer* buf, TimeStamp ts)
+{
+    string message = buf->retrieveAllAsString();
+    cout << "received: '" << message << "' from server" << endl;
+}
+
+void OnConnection(TcpConnectionPtr connPtr, int conn_fd, const SA_IN* peer_addr)
+{
+    {
+    MutexLockGuard lock(mutex);
+    tcpconnection = connPtr;
+    }
+    latch.countdown();
+}
+
+void OnDisconnection(TcpConnectionPtr connPtr, int conn_fd, const SA_IN* peer_addr)
+{
+    MutexLockGuard lock(mutex);
+    tcpconnection.reset();
+}
+
+
+void getConsoleInputAndSend()
+{
+
+    latch.wait();
+
+    std::string str;
+    while(std::cin >> str)
+    {
+        TcpConnectionPtr connection;
+        {
+            MutexLockGuard lock(mutex);
+            if(tcpconnection){
+                connection = tcpconnection;
+            }
+        }
+        if(!connection){
+            break;
+        }
+        connection->send(str);
+    }
+}
 
 int main(int argc, char* argv[])
+{
+    if(argc != 3){
+        printf("usage: ./xxx [port] [server name]\n");
+        return 0;
+    }
+
+    int port = stoi(std::string(argv[1]));
+
+    base::Thread thread(getConsoleInputAndSend);
+    thread.start();
+
+    EventLoop* loop = new EventLoop();
+    TcpClient client(port, argv[2], loop);
+    client.setOnConnectionCallback(OnConnection);
+    client.setOnMessageCallback(OnMessage);
+    client.setOnDisconnectedCb(OnDisconnection);
+
+    client.start();
+    loop->StartLoop();
+
+    return 0;
+}
+
+
+int main1(int argc, char* argv[])
 {
     if(argc != 2){
         printf("usage: ./xxx [port]\n");
--- apps/example/echo_server.cpp
+++ apps/example/echo_server.cpp
@@ -1,21 +1,38 @@
+// Author : Lance @ nwpu
+// echo_server that echo client data
 #include "LanceNet/base/Time.h"
 #include "LanceNet/net/Buffer.h"
 #include "LanceNet/net/EventLoop.h"
 #include "LanceNet/net/TcpConnection.h"
 #include <LanceNet/net/TcpServer.h>
+
+#include <iostream>
 #include <unistd.h>
 
 using namespace LanceNet;
 using namespace net;
+using namespace std;
+using TcpConnectionPtr = TcpServer::TcpConnectionPtr;
 
 EventLoop* gloop;
 
 void OnMessage(TcpConnection::TcpConnectionPtr conn, Buffer* buffer, TimeStamp ts)
 {
-    ::sleep(4);
     //echo
     std::string res = buffer->retrieveAllAsString();
+    cout << "server received: " << res << endl;
     conn->send(res);
+}
+
+
+void OnConnection(TcpConnectionPtr connPtr, int conn_fd, const SA_IN* peer_addr)
+{
+
+    char ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &peer_addr->sin_addr, ip, sizeof(ip));
+    int port = ntohs(peer_addr->sin_port);
+
+    cout << "Got connection from" << ip << ":" << port << endl;
 }
 
 
@@ -26,7 +43,9 @@ int main()
     TcpServer echo_server(gloop, 3456);
     echo_server.start();
 
+    echo_server.setNewConnectionCb(OnConnection);
     echo_server.setMessageCb(OnMessage);
+
     gloop->StartLoop();
 
     return 0;
