db6839e5f81b277db6810f7744694c07d6a36c9f
Lang <transplus@foxmail.com>
2023-07-22 10:22:14 +0800

    High performance Multi-threaded Echo Server v1.0 : 801862.6 qps using fortio benchmark tool

--- apps/echo/echo_server.cpp
+++ apps/echo/echo_server.cpp
@@ -1,5 +1,6 @@
 // Author : Lance @ nwpu
 // echo_server that echo client data
+#include "LanceNet/base/Logging.h"
 #include "LanceNet/base/Time.h"
 #include "LanceNet/net/Buffer.h"
 #include "LanceNet/net/EventLoop.h"
@@ -16,38 +17,35 @@ using TcpConnectionPtr = TcpServer::TcpConnectionPtr;
 
 EventLoop* gloop;
 
-void OnMessage(TcpConnection::TcpConnectionPtr conn, Buffer* buffer, TimeStamp ts)
+
+void OnMessage(const TcpConnection::TcpConnectionPtr& conn, Buffer* buffer, TimeStamp ts)
 {
     //echo
-    std::string res = buffer->retrieveAllAsString();
-    cout << "server received: " << res << endl;
-    conn->send(res);
+    // std::string res = buffer->retrieveAllAsString();
+    // conn->send(res);
+
+    conn->send(buffer);
 }
 
 
-void OnConnection(TcpConnectionPtr connPtr, int conn_fd, const SA_IN* peer_addr)
+void OnConnection(const TcpConnectionPtr& connPtr, int conn_fd, const SA_IN* peer_addr)
 {
-
-    char ip[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &peer_addr->sin_addr, ip, sizeof(ip));
-    int port = ntohs(peer_addr->sin_port);
-
-    cout << "Got connection from" << ip << ":" << port << endl;
+    // cout << "Got connection from" << connPtr->peerIpPort() << endl;
 }
 
 
 int main()
 {
+    Logger::setLogLevel(Logger::LogLevel::DEBUG);
     gloop = new EventLoop;
 
-    TcpServer echo_server(gloop, 3456);
+    TcpServer echo_server(gloop, 3456, 8); // 4 threads
     echo_server.start();
 
     echo_server.setOnNewConnectionCb(OnConnection);
     echo_server.setOnMessageCb(OnMessage);
 
     gloop->StartLoop();
-
     return 0;
 }
 
